bce60373bde5ddbac40d962b6041a770b6af3d59
Thomas de Almeida Reichmann <thomasar@triad.spo>
2019-10-29 13:04:32 -0300

    Mudança do tempo da cronjob

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
-const {Kayn, REGIONS} = require('kayn')
+const {
+    Kayn,
+    REGIONS
+} = require('kayn')
 const api = Kayn(process.env.LEAGUE_API_KEY)({
     region: REGIONS.BRAZIL
 })
 
 var schedule = require('node-schedule');
- 
-let j = schedule.scheduleJob('12 * * * *', () => checkName())
+
+let j = schedule.scheduleJob('3 * * * * *', () => checkName())
 console.log('Started')
 
 function checkName() {
     console.log(`Name checked at: ${Date.now()}`)
     api.Summoner.by.name('Thomas')
-    .then((summoner) => {
-        
-    })
-    .catch(err => {
-        if(err.statusCode == 404) {
-            console.log(`Jogador nao encontrado, Nome disponivel, mandando email`);
-            sendAlert()
-        }
-    })
+        .then((summoner) => {
+
+        })
+        .catch(err => {
+            if (err.statusCode == 404) {
+                console.log(`Jogador nao encontrado, Nome disponivel, mandando email`);
+                sendAlert()
+            }
+        })
 }
 
 async function sendAlert() {
     const nodemailer = require('nodemailer');
-    
+
     let transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: 587,
@@ -36,9 +39,9 @@ async function sendAlert() {
     });
 
     let info = await transporter.sendMail({
-        from: '"Auto Checker" <thomasarojsdfskdfn@gmail.com>', 
+        from: '"Auto Checker" <thomasarojsdfskdfn@gmail.com>',
         to: 'thomasarojsdfskdfn@gmail.com',
-        subject: 'Nome disponivel', 
+        subject: 'Nome disponivel',
         text: 'Nome thomas disponivel.'
     });
 
